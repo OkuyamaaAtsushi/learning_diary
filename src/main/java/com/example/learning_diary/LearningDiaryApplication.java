1775430fbac1c56d18d9791ab1ce9d36ed3ea8e4
KoheiYamafuku <kohei.yamafuku@intra.remtiro.com>
2025-04-03 16:53:54 +0900

    新規作成

---
+++ src/main/java/com/example/learning_diary/LearningDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learning_diary/LearningDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
